54b4d61c3e90fe79ec5928e6fb926f8dc4bcdd88
Adrian Wiewióra <93733010+AdrianWiewiora@users.noreply.github.com>
2022-11-20 11:14:57 +0100

    0.0.16
    0.0.16

--- src/GameState.cpp
+++ src/GameState.cpp
@@ -1,5 +1,7 @@
 #include "GameState.h"
 
+int GameState::mDifficultLevel = 1;
+
 GameState *GameState::getQueuedGameState() {
     return m_queued_game_state;
 }
--- src/GameState.h
+++ src/GameState.h
@@ -6,7 +6,7 @@ protected:
   GameState *m_queued_game_state = nullptr;
 
 public:
-    int mDifficultLevel = 1;
+    static int mDifficultLevel;
     virtual void Render(sf::RenderWindow *window) = 0;
     virtual void Input(sf::RenderWindow *window) = 0;
     virtual void Update(sf::RenderWindow *window, float delta) = 0;
